1a242161d8e82766c7feb52cf34d6812d6ddeb3e
mrudul21 <125190405+mrudul21@users.noreply.github.com>
2023-10-10 22:50:08 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+
+signup.addEventListener("click", () => {
+	slider.classList.add("moveslider");
+	formSection.classList.add("form-section-move");
+});
+
+login.addEventListener("click", () => {
+	slider.classList.remove("moveslider");
+	formSection.classList.remove("form-section-move");
+});
+if (loginSuccessful) {
+    window.location.href = "/login.html"; // Replace with the actual path to your home page
+}
